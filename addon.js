335604f44665ed7057d6ae342e877c1668d64415
Bork0J <102439518+Borko-Jovanovic@users.noreply.github.com>
2026-03-02 14:37:49 +0000

    Load csv links

--- addon.js
+++ addon.js
@@ -5,8 +5,8 @@ const manifest = {
     "id": "org.stremio.helloworld",
     "version": "1.0.0",
 
-    "name": "Hello World Addon",
-    "description": "Sample addon providing a few public domain movies",
+    "name": "Sinhronizovani crtaći V1",
+    "description": "Sinhronizovani crtaći - Vercel",
 
     // set what type of resources we will return
     "resources": [
@@ -20,11 +20,7 @@ const manifest = {
     "catalogs": [
         {
             type: 'movie',
-            id: 'helloworldmovies'
-        },
-        {
-            type: 'series',
-            id: 'helloworldseries'
+            id: 'Sinhronizovani crtaći - Filmovi'
         }
     ],
 
@@ -34,21 +30,29 @@ const manifest = {
 };
 
 const dataset = {
-    // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
-    "tt0032138": { name: "The Wizard of Oz", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
-    "tt0017136": { name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
-
-    // night of the living dead, example from magnet
-    "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
-    "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
 
     "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
-    "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
-
-    "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
-
-    "tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
 };
+
+//Read links.csv and add ID, Filename and URL, with default type "movie" to the dataset object
+const fs = require("fs");
+const csv = require("csv-parser");
+
+fs.createReadStream("links.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+        const id = row.ID;
+        const name = row.Filename;
+        const url = "https://sootiofortheweebs.midnightignite.me/resolve/realdebrid/4LO3ZE6LJOHHDF2TF4HZY7ECY4M46UUQYOXQSPBJ4Q3X5YHFEMEA/" + encodeURIComponent(row.URL);
+        dataset[id] = { name: name, type: "movie", url: url };
+    })
+    .on("end", () => {
+        console.log("CSV file successfully processed");
+    }); 
+
+
+
+
 
 // utility function to add from magnet
 function fromMagnet(name, type, uri) {
