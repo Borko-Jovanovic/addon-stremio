7aa2694de1f957f181c904c3fc6317f401ce7a30
Bork0J <102439518+Borko-Jovanovic@users.noreply.github.com>
2026-03-04 08:19:33 +0000

    netlify expose files

---
+++ netlify/functions/addon.js
@@ -0,0 +1,109 @@
+const { addonBuilder } = require("stremio-addon-sdk");
+const magnet = require("magnet-uri");
+
+const manifest = { 
+    "id": "org.stremio.helloworld",
+    "version": "1.0.0",
+
+    "name": "Sinhronizovani crtaći V1",
+    "description": "Sinhronizovani crtaći - Vercel",
+
+    // set what type of resources we will return
+    "resources": [
+        "catalog",
+        "stream"
+    ],
+
+    "types": ["movie", "series"], // your add-on will be preferred for those content types
+
+    // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+    "catalogs": [
+        {
+            type: 'movie',
+            id: 'Sinhronizovani crtaći - Filmovi'
+        }
+    ],
+
+    // prefix of item IDs (ie: "tt0032138")
+    "idPrefixes": [ "tt" ]
+
+};
+
+const dataset = {
+
+    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
+};
+
+//Read links.csv and add ID, Filename and URL, with default type "movie" to the dataset object
+const fs = require("fs");
+const csv = require("csv-parser");
+
+fs.createReadStream("links.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+        const id = row.ID;
+        const name = row.Filename;
+        const url = "https://sootiofortheweebs.midnightignite.me/resolve/realdebrid/4LO3ZE6LJOHHDF2TF4HZY7ECY4M46UUQYOXQSPBJ4Q3X5YHFEMEA/" + encodeURIComponent(row.URL);
+        dataset[id] = { name: name, type: "movie", url: url };
+    })
+    .on("end", () => {
+        console.log("CSV file successfully processed");
+    }); 
+
+
+
+
+
+// utility function to add from magnet
+function fromMagnet(name, type, uri) {
+    const parsed = magnet.decode(uri);
+    const infoHash = parsed.infoHash.toLowerCase();
+    const tags = [];
+    if (uri.match(/720p/i)) tags.push("720p");
+    if (uri.match(/1080p/i)) tags.push("1080p");
+    return {
+        name: name,
+        type: type,
+        infoHash: infoHash,
+        sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
+        tag: tags,
+        title: tags[0], // show quality in the UI
+    }
+}
+
+const builder = new addonBuilder(manifest);
+
+// Streams handler
+builder.defineStreamHandler(function(args) {
+    if (dataset[args.id]) {
+        return Promise.resolve({ streams: [dataset[args.id]] });
+    } else {
+        return Promise.resolve({ streams: [] });
+    }
+})
+
+const METAHUB_URL = "https://images.metahub.space"
+
+const generateMetaPreview = function(value, key) {
+    // To provide basic meta for our movies for the catalog
+    // we'll fetch the poster from Stremio's MetaHub
+    // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
+    const imdbId = key.split(":")[0]
+    return {
+        id: imdbId,
+        type: value.type,
+        name: value.name,
+        poster: METAHUB_URL+"/poster/medium/"+imdbId+"/img",
+    }
+}
+
+builder.defineCatalogHandler(function(args, cb) {
+    // filter the dataset object and only take the requested type
+    const metas = Object.entries(dataset)
+	.filter(([_, value]) => value.type === args.type)
+	.map(([key, value]) => generateMetaPreview(value, key))
+
+    return Promise.resolve({ metas: metas })
+})
+
+module.exports = builder.getInterface()
